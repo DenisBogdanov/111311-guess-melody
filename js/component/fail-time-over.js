dcedb7ecf851f6b85d6aefcab93fc3b131d3c55c
Denis Bogdanov <bogdanov.math@gmail.com>
2018-08-21 23:33:16 +0300

    Divide into modules.

---
+++ js/component/fail-time-over.js
@@ -0,0 +1,18 @@
+import {render, changeScreen} from '../util';
+import welcome from './welcome';
+
+const template = `
+  <section class="result">
+    <div class="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"></div>
+    <h2 class="result__title">Увы и ах!</h2>
+    <p class="result__total result__total--fail">Время вышло! Вы не успели отгадать все мелодии</p>
+    <button class="result__replay" type="button">Попробовать ещё раз</button>
+  </section>`;
+
+const element = render(template);
+
+const uiReplayButton = element.querySelector(`.result__replay`);
+
+uiReplayButton.addEventListener(`click`, () => changeScreen(welcome));
+
+export default element;
